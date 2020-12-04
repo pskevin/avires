bacded51933fde6880ae9a3616eac84cf0951aee
Kavan Sikand <kvsikand@gmail.com>
2020-12-04 12:12:47 -0800

    closer to running

--- sim/l1_data_cache.cpp
+++ sim/l1_data_cache.cpp
@@ -64,3 +64,82 @@ bool L1DataCache::store_single(uint64_t addr)
     // profile[instId][counter]++;
     return l1d;
 }
+
+/*! RMR (rodric@gmail.com) 
+ *   - temporary work around because decstr()
+ *     casts 64 bit ints to 32 bit ones
+ */
+static string mydecstr(uint64_t v, uint64_t w)
+{
+  ostringstream o;
+  o.width(w);
+  o << v;
+  string str(o.str());
+  return str;
+}
+
+CACHE_BASE::CACHE_BASE(std::string name, uint64_t cacheSize, uint64_t lineSize, uint64_t associativity)
+    : _name(name),
+      _cacheSize(cacheSize),
+      _lineSize(lineSize),
+      _associativity(associativity),
+      _lineShift(FloorLog2(lineSize)),
+      _setIndexMask((cacheSize / (associativity * lineSize)) - 1)
+{
+
+  ASSERTX(IsPower2(_lineSize));
+  ASSERTX(IsPower2(_setIndexMask + 1));
+
+  for (uint64_t accessType = 0; accessType < ACCESS_TYPE_NUM; accessType++)
+  {
+    _access[accessType][false] = 0;
+    _access[accessType][true] = 0;
+  }
+}
+
+/*!    
+ *  @brief Stats output method
+ */
+
+string CACHE_BASE::StatsLong(string prefix, CACHE_TYPE cache_type) const
+{
+  const uint64_t headerWidth = 19;
+  const uint64_t numberWidth = 12;
+
+  string out;
+
+  out += prefix + _name + ":" + "\n";
+
+  if (cache_type != CACHE_TYPE_ICACHE)
+  {
+    for (uint64_t i = 0; i < ACCESS_TYPE_NUM; i++)
+    {
+      const ACCESS_TYPE accessType = ACCESS_TYPE(i);
+
+      std::string type(accessType == ACCESS_TYPE_LOAD ? "Load" : "Store");
+
+      out += prefix + ljstr(type + "-Hits:      ", headerWidth) + mydecstr(Hits(accessType), numberWidth) +
+             "  " + fltstr(100.0 * Hits(accessType) / Accesses(accessType), 2, 6) + "%\n";
+
+      out += prefix + ljstr(type + "-Misses:    ", headerWidth) + mydecstr(Misses(accessType), numberWidth) +
+             "  " + fltstr(100.0 * Misses(accessType) / Accesses(accessType), 2, 6) + "%\n";
+
+      out += prefix + ljstr(type + "-Accesses:  ", headerWidth) + mydecstr(Accesses(accessType), numberWidth) +
+             "  " + fltstr(100.0 * Accesses(accessType) / Accesses(accessType), 2, 6) + "%\n";
+
+      out += prefix + "\n";
+    }
+  }
+
+  out += prefix + ljstr("Total-Hits:      ", headerWidth) + mydecstr(Hits(), numberWidth) +
+         "  " + fltstr(100.0 * Hits() / Accesses(), 2, 6) + "%\n";
+
+  out += prefix + ljstr("Total-Misses:    ", headerWidth) + mydecstr(Misses(), numberWidth) +
+         "  " + fltstr(100.0 * Misses() / Accesses(), 2, 6) + "%\n";
+
+  out += prefix + ljstr("Total-Accesses:  ", headerWidth) + mydecstr(Accesses(), numberWidth) +
+         "  " + fltstr(100.0 * Accesses() / Accesses(), 2, 6) + "%\n";
+  out += "\n";
+
+  return out;
+}
