91bbf4c4932b97b108d4db4add6d161bd62fe76b
Kevin Sijo Puthusseri <kevinsijo@gmail.com>
2020-12-07 20:34:10 +0000

    GUPS with sequential and read/write switches

--- apps/gups/src/gups.c
+++ apps/gups/src/gups.c
@@ -1,21 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename:  gups.c
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  02/21/2018 02:36:27 PM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  YOUR NAME (), 
- *   Organization:  
- *
- * =====================================================================================
- */
-
 #define _GNU_SOURCE
 
 #include <stdlib.h>
@@ -36,27 +18,26 @@
 #include "timer.h"
 #include "gups.h"
 
-#define MAX_THREADS	64
-
-#ifdef HOTSPOT
-extern uint64_t hotset_start;
-extern double hotset_fraction;
-#endif
+#define MAX_THREADS 64
 
 uint64_t hot_start = 0;
-uint64_t hotsize;
 
-struct gups_args {
-  int tid;                      // thread id
-  uint64_t *indices;       // array of indices to access
-  void* field;                  // pointer to start of thread's region
-  uint64_t iters;          // iterations to perform
-  uint64_t size;           // size of region
-  uint64_t elt_size;       // size of elements
+#define SEQUENTIAL_ACCESS 0 // 0 = Random, 1 = Sequential
+#define WRITE_UPDATE 1      // 0 = Read, 1 = Write
+
+struct gups_args
+{
+  int tid; // thread id
+  // uint64_t *indices; // array of indices to access
+  void *field;       // pointer to start of thread's region
+  uint64_t iters;    // iterations to perform
+  uint64_t size;     // size of region
+  uint64_t elt_size; // size of elements
+  uint64_t hotsize;  //size of hotset
+  uint64_t hotstart; // start of hotset
 };
 
 static unsigned long updates, nelems;
-
 
 static uint64_t lfsr_fast(uint64_t lfsr)
 {
@@ -66,12 +47,10 @@ static uint64_t lfsr_fast(uint64_t lfsr)
   return lfsr;
 }
 
-
 static void *do_gups(void *arguments)
 {
-  //printf("do_gups entered\n");
-  struct gups_args *args = (struct gups_args*)arguments;
-  char *field = (char*)(args->field);
+  struct gups_args *args = (struct gups_args *)arguments;
+  char *field = (char *)(args->field);
   uint64_t i, j;
   uint64_t index1, index2;
   uint64_t elt_size = args->elt_size;
@@ -85,80 +64,84 @@ static void *do_gups(void *arguments)
   index1 = 0;
   index2 = 0;
 
-  for (i = 0; i < args->iters; i++) {
-    hot_num = lfsr % 4 + 8;
-    for (j = 0; j < hot_num; j++) {
-      lfsr = lfsr_fast(lfsr);
-      index1 = hot_start + (lfsr % hotsize);
-      memcpy(data, &field[index1 * elt_size], elt_size);
-      memset(data, data[0] + i, elt_size);
-      memcpy(&field[index1 * elt_size], data, elt_size);
-    }
-    i += hot_num;
+  for (i = 0; i < args->iters; i++)
+  {
+    if (args->hotsize > 0)
+    {
+#if SEQUENTIAL_ACCESS
+      hot_num = args->hotsize;
+#else
+      hot_num = lfsr % 4 + args->hotsize;
+#endif
 
+      for (j = 0; j < hot_num; j++)
+      {
+#if SEQUENTIAL_ACCESS
+          index1 = args->hotstart + j;
+#else
+        lfsr = lfsr_fast(lfsr);
+        index1 = args->hotstart + (lfsr % args->hotsize);
+#endif
+          // printf("\nhot %d", index1);
+          memcpy(data, &field[index1 * elt_size], elt_size);
+#if WRITE_UPDATE
+          memset(data, data[0] + i, elt_size);
+          memcpy(&field[index1 * elt_size], data, elt_size);
+#endif
+      }
+      i += hot_num;
+    }
+
+#if SEQUENTIAL_ACCESS
+    index2 = i % args->size;
+#else
     lfsr = lfsr_fast(lfsr);
     index2 = lfsr % (args->size);
+#endif
+
+    // printf("\nreg %d", index2);
     memcpy(data, &field[index2 * elt_size], elt_size);
+
+#if WRITE_UPDATE
     memset(data, data[0] + i, elt_size);
     memcpy(&field[index2 * elt_size], data, elt_size);
+#endif
   }
-/*
-  //printf("Thread [%d] starting: field: [%llx]\n", args->tid, field);
-  for (i = 0; i < args->iters; i++) {
-    index = args->indices[i];
-    memcpy(data, &field[index * elt_size], elt_size);
-    memset(data, data[0] + i, elt_size);
-    memcpy(&field[index * elt_size], data, elt_size);
-  }
-*/
   return 0;
 }
-/*
-static void *calc_indices_thread(void *arg)
-{
-  struct gups_args *gai = (struct gups_args*)arg;
-  calc_indices(gai->indices, updates, nelems);
-  return NULL;
-}
-*/
+
 int main(int argc, char **argv)
 {
   int threads;
-  unsigned long expt;
+  unsigned long expt, hotset_frac, hotstart_frac;
   unsigned long size, elt_size;
-  struct timeval starttime, stoptime;
-  double secs, gups;
+  uint64_t hotsize, hotstart;
+
   int i;
   void *p;
-  struct gups_args** ga;
+  struct gups_args **ga;
   pthread_t t[MAX_THREADS];
 
   pid_t pid = getpid();
+  printf("\npid: %d\n", pid);
 
-  printf("\npid: %d\n",  pid);
-  
-  argv[1]="1";
-  argv[2]="1000000";
-  argv[3]="33";
-  argv[4]="8";
-
-  argc = 5;
-
-  if (argc != 5) {
-    printf("Usage: %s [threads] [updates per thread] [exponent] [data size (bytes)] [noremap/remap]\n", argv[0]);
+  if (argc != 7)
+  {
+    printf("Usage: %s [threads] [updates per thread] [exponent] [data size (bytes)] [hotset start (\%)] [hotset size (\%)]\n", argv[0]);
     printf("  threads\t\t\tnumber of threads to launch\n");
     printf("  updates per thread\t\tnumber of updates per thread\n");
     printf("  exponent\t\t\tlog size of region\n");
     printf("  data size\t\t\tsize of data in array (in bytes)\n");
+    printf("  hotset start\t\t\tstarting from (in percentage)\n");
+    printf("  hotset size\t\t\tnumber of elements (in percentage)\n");
+
     return 0;
   }
 
-  gettimeofday(&starttime, NULL);
-  
   threads = atoi(argv[1]);
   assert(threads <= MAX_THREADS);
-  ga = (struct gups_args**)malloc(threads * sizeof(struct gups_args*));
-  
+  ga = (struct gups_args **)malloc(threads * sizeof(struct gups_args *));
+
   updates = atol(argv[2]);
   updates -= updates % 256;
   expt = atoi(argv[3]);
@@ -168,102 +151,62 @@ int main(int argc, char **argv)
   size -= (size % 256);
   assert(size > 0 && (size % 256 == 0));
   elt_size = atoi(argv[4]);
+  nelems = (size / threads) / elt_size; // number of elements per thread
+  hotstart_frac = atoi(argv[5]);
+  hotset_frac = atoi(argv[6]);
+  assert(hotstart_frac >= 0 && hotstart_frac <= 100);
+  assert(hotset_frac + hotstart_frac >= 0 && hotset_frac + hotstart_frac <= 100);
+  assert(hotset_frac >= 0 && hotset_frac <= 100);
+  hotstart = (uint64_t)nelems * hotstart_frac / 100 - 1;
+  hotsize = (uint64_t)nelems * hotset_frac / 100;
 
-  printf("%lu updates per thread (%d threads)\n", updates, threads);
   printf("field of 2^%lu (%lu) bytes\n", expt, size);
+  printf("%lu updates per thread (%d threads)\n", updates, threads);
   printf("%ld byte element size (%ld elements total)\n", elt_size, size / elt_size);
+  printf("Elements per thread: %lu\n", nelems);
+  printf("Hotset starts at %lu of size %lu\n", hotstart, hotsize);
 
   p = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
-  gettimeofday(&stoptime, NULL);
-  printf("Init took %.4f seconds\n", elapsed(&starttime, &stoptime));
   printf("Region address: %p\t size: %ld\n", p, size);
   printf("Field addr: 0x%x\n", p);
 
-  nelems = (size / threads) / elt_size; // number of elements per thread
-  printf("Elements per thread: %lu\n", nelems);
-
-  printf("initializing thread data\n");
-  for (i = 0; i < threads; ++i) {
-    ga[i] = (struct gups_args*)malloc(sizeof(struct gups_args));
+  printf("Initializing thread data\n");
+  for (i = 0; i < threads; ++i)
+  {
+    ga[i] = (struct gups_args *)malloc(sizeof(struct gups_args));
     ga[i]->field = p + (i * nelems * elt_size);
-    //printf("Thread [%d] starting address: %llx\n", i, ga[i]->field);
-    //printf("thread %d start address: %llu\n", i, (unsigned long)td[i].field);
-    /*
-    ga[i]->indices = (unsigned long*)malloc(updates * sizeof(unsigned long));
-    if (ga[i]->indices == NULL) {
-      perror("malloc");
-      exit(1);
-    }
-    int r = pthread_create(&t[i], NULL, calc_indices_thread, (void*)ga[i]);
-    assert(r == 0); 
-    */
   }
-  
-  // wait for worker threads
-  //for (i = 0; i < threads; i++) {
-    //int r = pthread_join(t[i], NULL);
-    //assert(r == 0);
-  //}
 
-  hot_start = 0;
-  hotsize = nelems / 10;
-  printf("hot_start: %lu\thot_size: %lu\n", hot_start, hotsize);
-
-  printf("Timing.\n");
   // spawn gups worker threads
-  for (i = 0; i < threads; i++) {
+  for (i = 0; i < threads; i++)
+  {
     //printf("starting thread [%d]\n", i);
-    ga[i]->tid = i; 
+    ga[i]->tid = i;
     ga[i]->iters = updates;
     ga[i]->size = nelems;
     ga[i]->elt_size = elt_size;
-     int r = pthread_create(&t[i], NULL, do_gups, (void*)ga[i]);
+    ga[i]->hotstart = hotstart;
+    ga[i]->hotsize = hotsize;
+    int r = pthread_create(&t[i], NULL, do_gups, (void *)ga[i]);
     assert(r == 0);
   }
 
   // wait for worker threads
-  for (i = 0; i < threads; i++) {
+  for (i = 0; i < threads; i++)
+  {
     int r = pthread_join(t[i], NULL);
     assert(r == 0);
   }
-  gups = threads * ((double)updates) / (secs * 1.0e9);
-  printf("GUPS = %.10f\n", gups);
+  printf("Finished running GUPS\n");
 
-#ifdef HOTSPOT
-  hot_start = nelems - (uint64_t)(hotsize) - 1;
-  printf("hot_start: %lu\thot_size: %lu\n", hot_start, hotsize);
-
-  printf("Timing.\n");
-  
-  // spawn gups worker threads
-  for (i = 0; i < threads; i++) {
-    ga[i]->tid = i; 
-    ga[i]->iters = updates;
-    ga[i]->size = nelems;
-    ga[i]->elt_size = elt_size;
-    int r = pthread_create(&t[i], NULL, do_gups, (void*)ga[i]);
-    assert(r == 0);
-  }
-
-  // wait for worker threads
-  for (i = 0; i < threads; i++) {
-    int r = pthread_join(t[i], NULL);
-    assert(r == 0);
-  }
-
-  gups = threads * ((double)updates) / (secs * 1.0e9);
-  printf("GUPS = %.10f\n", gups);
-#endif
-  
-  for (i = 0; i < threads; i++) {
-    free(ga[i]->indices);
+  for (i = 0; i < threads; i++)
+  {
     free(ga[i]);
   }
   free(ga);
-  
-  //munmap(p, size);
-  // int testInteger;
-  // scanf("%d", &testInteger);  
+
+  munmap(p, size);
+
   return 0;
 }
