f75f773dd239cc4a75d8ce6a577d7e1388b15d61
Kevin Sijo Puthusseri <kevinsijo@gmail.com>
2020-12-08 00:33:59 +0000

    adding memory access patterns data

--- bench/run_hist.py
+++ bench/run_hist.py
@@ -30,13 +30,13 @@ def run_experiment(cmd_str, tries=math.inf):
             break
 
 if __name__ == '__main__':
-    pin = 'pin -t ../sim/obj-intel64/mem_trace.so -o ./results/hist/rndm_write/gups_rndm_write_ --'
+    pin = 'pin -t ../sim/obj-intel64/mem_trace.so -o ./results/hist/hotset/gups_hotset_ --'
 
     gups_bin = '../apps/gups/obj/gups_rndm_write.o'
     gups_configs =[
         # '1 1000 15 8 0 0',
         # '1 10000 15 8 0 0',
-        '1 100000 15 8 0 0',
+        '1 100000 15 8 45 10',
         # '1 1000000 15 8 0 0',
         # '1 10000000 15 8 0 0',
         # '1 100000000 15 8 0 0',
