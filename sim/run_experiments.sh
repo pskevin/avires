8ccc2664c83e2580b4d1b27ef3d3d09b64bd231a
Kavan Sikand <kvsikand@gmail.com>
2020-12-08 12:19:42 -0800

    ffix constants

--- sim/run_experiments.sh
+++ sim/run_experiments.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pin -t obj-intel64/mem_trace.so -o experiments/sim_small_linux_ -mm 1 -- ../apps/gups/obj/gups.o 1 100000 9 8
+pin -t obj-intel64/mem_trace.so -o experiments/sim_small_linux_ -mm 1 -- ../apps/gups/obj/gups.o 1 100000 15 8 0 0 0
 
 # pin -t obj-intel64/mem_trace.so -o experiments/sim_small_linux_ -mm 1 -- ../apps/gups/obj/gups.o 1 10000 9 8
-pin -t obj-intel64/mem_trace.so -o experiments/sim_small_ -- ../apps/gups/obj/gups.o 1 100000 9 8
+pin -t obj-intel64/mem_trace.so -o experiments/sim_small_ -- ../apps/gups/obj/gups.o 1 100000 15 8 0 0 0
