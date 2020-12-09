1f00d3df46e5d39bbbaa79354dcf28e3fb843483
Kavan Sikand <kvsikand@gmail.com>
2020-12-08 18:21:27 -0800

    fix script

--- sim/run_experiments.sh
+++ sim/run_experiments.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+# Run on linux memory manager
 pin -t obj-intel64/mem_trace.so -o experiments/sim_small_linux_ -mm 1 -- ../apps/gups/obj/gups.o 1 100000 15 8 0 0 0
 
-# pin -t obj-intel64/mem_trace.so -o experiments/sim_small_linux_ -mm 1 -- ../apps/gups/obj/gups.o 1 10000 9 8
-pin -t obj-intel64/mem_trace.so -o experiments/sim_small_ -- ../apps/gups/obj/gups.o 1 100000 15 8 0 0 0
+# Run application with hotset on simple memory manager
+pin -t obj-intel64/mem_trace.so -o experiments/sim_small_ -- ../apps/gups/obj/gups.o 1 100000 15 8 40 30 90
